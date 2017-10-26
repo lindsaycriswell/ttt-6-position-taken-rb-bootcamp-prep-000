30577080c9b445245159d5203a0219805cea0902
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-26 00:10:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,6 @@ def position_taken?(board, index)
   if board[index-1] == "  " || board[index-1] == " " || board[index-1] == nil
     false
   elsif board[index-1] == "X" || board[index-1] == "O"
+    true
   end
 end
